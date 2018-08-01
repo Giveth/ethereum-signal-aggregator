31770f3e049550557f6c245a79f8bb510355f5f6
deam <deamme@users.noreply.github.com>
2018-08-01 23:50:46 +0200

    Fix server

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = routes.getRequestHandler(app)
 
-const server = express()
+app.prepare().then(() => {
+  const server = express()
 
-// serve general static files
-server.use('/static', express.static(path.join(__dirname, '/static')))
+  // serve general static files
+  server.use('/static', express.static(path.join(__dirname, '/static')))
 
-// use next.js
-server.get('*', (req, res) => handle(req, res))
+  // use next.js
+  server.get('*', (req, res) => handle(req, res))
 
-server.listen(3000, err => {
-  if (err) throw err
-  console.log('> Ready on http://localhost:3000')
+  server.listen(3000, err => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
 })
