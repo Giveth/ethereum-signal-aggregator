a626c83dfdb307ca746286019c2e50e7ce1c37da
deam <deamme@users.noreply.github.com>
2018-08-03 23:46:34 +0200

    Added jsx-a11y lint support and initial docz setup

---
+++ components/Box.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import {
+  height,
+  width,
+  space,
+  color,
+  flex,
+  flexDirection,
+  flexWrap,
+  display,
+  alignSelf,
+  alignItems,
+  justifyContent,
+} from 'styled-system'
+
+export default styled.div`
+  ${height}
+  ${width}
+  ${space}
+  ${color}
+  ${flex}
+  ${flexDirection}
+  ${flexWrap}
+  ${display}
+  ${alignSelf}
+  ${alignItems}
+  ${justifyContent}
+`
---
+++ components/Layout.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import Box from './Box'
+
+export default styled(Box)`
+  max-width: 64em;
+  margin: 0 auto;
+`
