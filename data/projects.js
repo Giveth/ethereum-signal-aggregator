782bc92390c4f879b98e12faa3ebae1e0ac84d14
deam <deamme@users.noreply.github.com>
2018-08-04 19:16:17 +0200

    Initial data specification

---
+++ data/projects.js
@@ -0,0 +1,6 @@
+export default [
+  {
+    title: 'Giveth Galaxy',
+    projects: ['some github repository url', 'another github repository url'],
+  },
+]
---
+++ data/stakeholders.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    index: 1,
+    title: 'name',
+    description: 'some text',
+    tags: ['dev', 'influencer'],
+    links: ['some url', 'another url'],
+    proposals: [
+      {
+        id: 999,
+        stance: 'either',
+        sources: ['some url', 'another url'],
+      },
+    ],
+  },
+]
