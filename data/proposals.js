58d174046e73fe8520c4550d418f9f8fedf13102
deam <deamme@users.noreply.github.com>
2018-08-16 17:11:37 +0200

    Added more dummy data

--- data/proposals.js
+++ data/proposals.js
@@ -10,4 +10,48 @@ export default [
     created: '2018-04-04',
     requires: [],
   },
+  {
+    id: 999,
+    title:
+      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
+    authors: ['Afri Schoedon (@5chdn)'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2018-04-04',
+    requires: [],
+  },
+  {
+    id: 999,
+    title:
+      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
+    authors: ['Afri Schoedon (@5chdn)'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2018-04-04',
+    requires: [],
+  },
+  {
+    id: 999,
+    title:
+      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
+    authors: ['Afri Schoedon (@5chdn)'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2018-04-04',
+    requires: [],
+  },
+  {
+    id: 999,
+    title:
+      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
+    authors: ['Afri Schoedon (@5chdn)'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2018-04-04',
+    requires: [],
+  },
 ]
