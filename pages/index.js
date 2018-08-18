0471497fbae44ad0c15bc70e77d629b92879fa4f
deam <deamkh@gmail.com>
2018-08-18 19:29:45 +0200

    Added more EIP details to card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 import { Trail, animated } from 'react-spring'
+import { Link } from '../components/Icons'
+import Badge from '../components/Badge'
 import Box from '../components/Box'
 import Layout from '../components/Layout'
 import Navbar from '../components/Navbar'
@@ -11,9 +13,45 @@ import proposals from '../data/proposals'
 
 const items = proposals.map(proposal => (
   <Card p={3} m={3} width="300px">
-    <Text textAlign="center" fontWeight="500">
-      {proposal.title}
-    </Text>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      borderBottom="1px solid #DDDDDD"
+      pb={2}
+    >
+      <Text color="#0A1F44" fontWeight="500" fontSize="24px">
+        EIP
+        {proposal.id}
+      </Text>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100px"
+      >
+        <Badge
+          bg="#F7BA44"
+          border="2px solid #D19829"
+          borderRadius="1rem"
+          color="white"
+          text="DRAFT"
+        />
+        <Link />
+      </Box>
+    </Box>
+    <Box display="flex" flexDirection="column" pt={2}>
+      <Badge
+        bg="#BBBBBB"
+        border="2px solid #888888"
+        borderRadius=".25rem"
+        color="white"
+        text="Core"
+        width="60px"
+        mb={2}
+      />
+      <Text color="#8A94A6" fontWeight="500">{proposal.title}</Text>
+    </Box>
   </Card>
 ))
 
@@ -31,9 +69,9 @@ export default class App extends Component {
 
     return (
       <div>
-        <Navbar />
-        <Layout mt={3} display="flex" justifyContent="center">
-          <Box display="flex" flexWrap="wrap" justifyContent="space-between">
+        <Navbar activeIndex={0} />
+        <Layout display="flex" justifyContent="center">
+          <Box display="flex" flexWrap="wrap">
             <Trail
               native
               from={{ opacity: 0 }}
