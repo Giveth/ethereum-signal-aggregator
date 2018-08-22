e49242b058169256cbf99e01b45b8f152c3063ed
James Hancock <james@promethios.io>
2018-08-22 17:22:11 -0400

    added eips to proposals.ks
    added 10 or so to test filtering.

--- data/proposals.js
+++ data/proposals.js
@@ -1,57 +1,146 @@
 export default [
-  {
-    id: 999,
+   {
+    id: 999999,
     title:
-      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
-    authors: ['Afri Schoedon (@5chdn)'],
-    status: 'draft',
+      'template',
+    authors: [''],
+    status: '',
     type: 'standards track',
-    category: 'core',
-    created: '2018-04-04',
+    category: '',
+    created: '',
     requires: [],
+	visible: False
   },
   {
-    id: 999,
+    id: 2,
     title:
-      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
-    authors: ['Afri Schoedon (@5chdn)'],
-    status: 'draft',
+      'Homestead Hard-fork Changes',
+    authors: ['Vitalik Buterin'],
+    status: 'final',
     type: 'standards track',
     category: 'core',
-    created: '2018-04-04',
+    created: '2015-11-15',
     requires: [],
+	visible: True
   },
   {
-    id: 999,
+    id: 3,
     title:
-      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
-    authors: ['Afri Schoedon (@5chdn)'],
-    status: 'draft',
+      'Addition of CALLDEPTH opcode',
+    authors: ['Martin Holst Swende'],
+    status: 'deferred',
     type: 'standards track',
     category: 'core',
-    created: '2018-04-04',
+    created: '2015-11-19',
     requires: [],
+	visible: True
   },
   {
-    id: 999,
+    id: 4,
     title:
-      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
-    authors: ['Afri Schoedon (@5chdn)'],
+      'EIP Classification',
+    authors: ['Joseph Chow'],
     status: 'draft',
     type: 'standards track',
-    category: 'core',
-    created: '2018-04-04',
+    category: 'meta',
+    created: '2015-11-17',
     requires: [],
+	visible: True
   },
   {
-    id: 999,
+    id: 5,
     title:
-      'Restore Contract Code at 0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4',
-    authors: ['Afri Schoedon (@5chdn)'],
+      'Gas Usage for `RETURN` and `CALL*`',
+    authors: ['Christian Reitwiessner'],
     status: 'draft',
     type: 'standards track',
     category: 'core',
-    created: '2018-04-04',
+    created: '2015-11-22',
     requires: [],
+	visible: True
+  },
+  {
+    id: 6,
+    title:
+      'Renaming SUICIDE opcode',
+    authors: ['Hudson Jameson'],
+    status: 'final',
+    type: 'standards track',
+    category: 'interface',
+    created: '2015-11-22',
+    requires: [],
+	visible: True
+  },
+  {
+    id: 7,
+    title:
+      'DELEGATECALL',
+    authors: ['Vitalik Buterin'],
+    status: 'final',
+    type: 'standards track',
+    category: 'core',
+    created: '2015-11-15',
+    requires: [],
+	visible: True
+  },
+  {
+    id: 8,
+    title:
+      'devp2p Forward Compatibility Requirements for Homestead',
+    authors: ['Felix Lange'],
+    status: 'final',
+    type: 'standards track',
+    category: 'networking',
+    created: '2015-12-18',
+    requires: [],
+	visible: True
+  },
+   {
+    id: 86,
+    title:
+      'Abstraction of transaction origin and signature',
+    authors: ['Vitalik Buterin'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2017-02-10',
+    requires: [],
+	visible: True
+  },
+  {
+    id: 101,
+    title:
+      'Serenity Currency and Crypto Abstraction',
+    authors: ['Vitalik Buterin'],
+    status: 'draft',
+    type: 'standards track',
+    category: 'core',
+    created: '2015-11-15',
+    requires: [],
+	visible: True
+  },
+  {
+    id: 145,
+    title:
+      'Bitwise shifting instructions in EVM',
+    authors: ['Alex Beregszaszi, Paweł Bylica'],
+    status: 'accepted',
+    type: 'standards track',
+    category: 'core',
+    created: '2017-02-13',
+    requires: [],
+	visible: True
+  },
+  {
+    id: 1167,
+    title:
+      'Minimal Proxy Contract',
+    authors: ['Peter Murray, Nate Welch, Joe Messerman'],
+    status: 'last-call',
+    type: 'standards track',
+    category: 'ERC',
+    created: '2018-06-22',
+    requires: [],
+	visible: True
   },
 ]
