72664d8a6cd1ceac710ed87dfb23792d42464b8a
deam <deamkh@gmail.com>
2018-08-26 19:42:12 +0200

    Update projects data

--- data/projects.js
+++ data/projects.js
@@ -3,4 +3,12 @@ export default [
     title: 'Giveth Galaxy',
     projects: ['some github repository url', 'another github repository url'],
   },
+  {
+    title: 'Aragon',
+    projects: ['some github repository url', 'another github repository url'],
+  },
+  {
+    title: 'Status',
+    projects: ['some github repository url', 'another github repository url'],
+  },
 ]
